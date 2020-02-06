d319d640b6afb9e403dc83370dca186088c9a76d
Aaron Diable <aaron.diable@gmail.com>
2020-02-06 13:01:39 +0000

    Commit before adding new paths

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
 
 
 def all_blogs(req):
-    return render(req, 'blog/all_blogs.html')
+    blogs = Blog.objects.order_by('-date')[:3]
+    return render(req, 'blog/all_blogs.html', {
+        'blog': blogs
+    })
